1ab15bd40e6d1807905f7214051b609e67fa5ef6
cramanan <cramananjaonapro@gmail.com>
2026-04-19 02:22:00 +0200

    refactor: refactored config

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, import.meta.dirname);
-
+  const port = 5173;
   return {
     build: { outDir: "internal/resources/vite/build" },
     clearScreen: false,
     resolve: {
       alias: {
-        "@": path.resolve("src"),
+        "@": path.resolve(import.meta.dirname, "src"),
       },
     },
     server: {
-      port: 5173,
-      origin: "http://localhost:5173",
+      port,
+      origin: `http://localhost:${port}`,
       proxy: { "/api": { target: env.VITE_API_URL, changeOrigin: true } },
     },
   };
