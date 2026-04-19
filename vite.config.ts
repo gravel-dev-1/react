a35ba2e58b4b5b982ddc58f5b6594a6c2c4df910
cramanan <cramananjaonapro@gmail.com>
2026-04-19 02:53:27 +0200

    feat added React scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, import.meta.dirname);
@@ -17,5 +18,6 @@ export default defineConfig(({ mode }) => {
       origin: `http://localhost:${port}`,
       proxy: { "/api": { target: env.VITE_API_URL, changeOrigin: true } },
     },
+    plugins: [react()],
   };
 });
